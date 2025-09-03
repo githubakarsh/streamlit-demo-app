8a7b355c5288412ecd846c94ee4d9885877127d1
Akarsh Saraff <saraffakarsh@gmail.com>
2025-09-03 11:26:59 +0530

    chore: upgrade React to 19

--- sidebar_component/sidebar_comp/frontend/src/index.tsx
+++ sidebar_component/sidebar_comp/frontend/src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import OnHoverTabs from "./OnHoverTabs"
 
-ReactDOM.render(
+const container = document.getElementById("root") as HTMLElement
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
-    <OnHoverTabs/>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <OnHoverTabs />
+  </React.StrictMode>
 )
