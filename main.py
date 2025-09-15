b1a8d15c29b77528cac1441199f4b0f1ff5d2457
Akarsh Saraff <saraffakarsh@gmail.com>
2025-09-15 16:39:29 +0530

    commit
    commit

--- main.py
+++ main.py
@@ -50,28 +50,28 @@ with st.sidebar:
 
     _selected_section = on_hover_tabs(menu_data=menu_data, key="main_nav_tabs")
 
-architecture_drift = st.Page("pages/dashboard/ArchitectureDrift.py", title="Architecture Drift", icon=":material/add_box:")
+# architecture_drift = st.Page("pages/dashboard/ArchitectureDrift.py", title="Architecture Drift", icon=":material/add_box:")
 
-hldd_generation = st.Page("pages/design/HLDD_Generation.py", title="HLDD Generation", icon=":material/add_box:")
-hldd_upgrade = st.Page("pages/design/HLDD_Upgrade.py", title="HLDD Upgrade", icon=":material/stat_3:")
+# hldd_generation = st.Page("pages/design/HLDD_Generation.py", title="HLDD Generation", icon=":material/add_box:")
+# hldd_upgrade = st.Page("pages/design/HLDD_Upgrade.py", title="HLDD Upgrade", icon=":material/stat_3:")
 
-hldd_standard = st.Page("pages/review/HLDD_Standard.py", title="HLDD Standard", icon=":material/receipt:")
-privileged_access = st.Page("pages/review/PrivilegedAccess.py", title="Privileged Access Management", icon=":material/login:")
-aws_well_architectured = st.Page("pages/review/AWS_Well_Architected.py", title="AWS Well Architected", icon=":material/domain:")
-adr_insight = st.Page("pages/review/ADR_Insight.py", title="ADR Insight", icon=":material/trip_origin:")
+# hldd_standard = st.Page("pages/review/HLDD_Standard.py", title="HLDD Standard", icon=":material/receipt:")
+# privileged_access = st.Page("pages/review/PrivilegedAccess.py", title="Privileged Access Management", icon=":material/login:")
+# aws_well_architectured = st.Page("pages/review/AWS_Well_Architected.py", title="AWS Well Architected", icon=":material/domain:")
+# adr_insight = st.Page("pages/review/ADR_Insight.py", title="ADR Insight", icon=":material/trip_origin:")
 
-chat_page = st.Page("pages/chat/Chatbot.py", title="Chat Assistant", icon=":material/chat_bubble:")
-
-
-pages = {
-    "Dashboard": [architecture_drift],
-    "Design": [hldd_generation, hldd_upgrade],
-    "Review": [hldd_standard, privileged_access, aws_well_architectured, adr_insight],
-    "Chat": [chat_page],
-}
+# chat_page = st.Page("pages/chat/Chatbot.py", title="Chat Assistant", icon=":material/chat_bubble:")
 
 
+# pages = {
+#     "Dashboard": [architecture_drift],
+#     "Design": [hldd_generation, hldd_upgrade],
+#     "Review": [hldd_standard, privileged_access, aws_well_architectured, adr_insight],
+#     "Chat": [chat_page],
+# }
 
-pg = st.navigation(pages)
 
-pg.run()
+
+# pg = st.navigation(pages)
+
+# pg.run()
